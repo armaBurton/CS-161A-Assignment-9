f743fd2d0b057ab6ab16322a6b4406a130e61ac7
Arma Burton <arma@armaburton.com>
2023-05-04 16:24:06 -0700

    had the loop working a minute ago and now it's not

--- readInt/readInt.cpp
+++ readInt/readInt.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int readInt(string prompt){
+  
   int number {};
+  do{
 
-  cout << endl 
-       << prompt << endl;
-  cin >> number;
+    cout << endl 
+         << prompt << endl;
+    cin >> number;
+
+    if (number < 0){
+      cout << "The number of assignment you have entered is too small.\n";
+    } else if (number > 10){
+      cout << "The number of assignments you have entered is too large.\n";
+    } 
+
+  } while ((10 < number < 0));
+
+  
 
   return number;
 }
