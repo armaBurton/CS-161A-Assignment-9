c10fd6c516aade96be2d704d0c0a9fa0e2d665bb
Arma Burton <arma@armaburton.com>
2023-05-04 16:52:46 -0700

    I'm not sure what is going on, but I'm going to move the error check to the main

--- readInt/readInt.cpp
+++ readInt/readInt.cpp
@@ -6,19 +6,23 @@ using namespace std;
 int readInt(string prompt){
   
   int number {};
+  // bool checkNumber {};
   do{
 
-    cout << endl 
-         << prompt << endl;
-    cin >> number;
-
-    if (number < 0){
-      cout << "The number of assignment you have entered is too small.\n";
-    } else if (number > 10){
-      cout << "The number of assignments you have entered is too large.\n";
-    } 
-      // cout << !isnan(number) << true << endl;
-  } while (number < 0 || number > 10);
+      cout << endl 
+           << prompt << endl;
+      cin >> number;
+  
+      if (number < 0){
+        cout << "The number of assignment you have entered is too small.\n";
+      } else if (number > 10){
+        cout << "The number of assignments you have entered is too large.\n";
+      } else if (!isnan(number)){
+        cout << "Please enter a number between 0 and 10\n";
+        number = readInt(prompt);
+      }
+        cout << !isnan(number) << true << endl;
+  } while (number < 0 || number > 10 && isnan(number) == true);
 
   
 
