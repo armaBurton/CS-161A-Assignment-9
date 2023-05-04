0146eee264a883fbcf0c79c03c1f86ff9c7d0c48
Arma Burton <arma@armaburton.com>
2023-05-04 14:13:12 -0700

    added space at top of text to distinguish it as a new console prompt

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@ int main () {
 }
 
 void welcomeText(){
-  cout << "Welcome to my Final Grade Calculator!\n"
+  cout << endl
+       << "Welcome to my Final Grade Calculator!\n"
        << "Please enter the following information and I will calculate your Final Numerical Grade and Letter Grade for you!\n"
        << "The number of assignments must be between 0 and 10\n"
        << "All scores entered must be between 0 and 4\n"
