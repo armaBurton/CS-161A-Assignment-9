dc75f15218525100663f9cde6b5f207269a0ea04
Arma Burton <arma@armaburton.com>
2023-05-04 17:15:34 -0700

    fixed the error check and the loop, was not intuitive at all.

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ int main () {
   int consoleInt {};
 
   welcomeText();
-  consoleInt = readInt("Enter the number of assignments (0 to 10): ");
-  cout << endl
-       << consoleInt << endl;
+    consoleInt = readInt("Enter the number of assignments (0 to 10): ");
+    cout << endl
+         << consoleInt << endl;
 
 
   return 0;
--- readInt/readInt.cpp
+++ readInt/readInt.cpp
@@ -1,28 +1,37 @@
 #include <iostream>
 #include <cmath>
+#include <limits>
 
 using namespace std;
 
 int readInt(string prompt){
   
-  int number {};
-  // bool checkNumber {};
+  int number = -1;
+  bool checkNumber {};
   do{
 
       cout << endl 
            << prompt << endl;
       cin >> number;
-  
+      while (1){
+        if (cin.fail()){
+          cin.clear();
+          cin.ignore(numeric_limits<streamsize>::max(),'\n');
+          cout << "Please enter a number between 0 - 10.\n";
+          cin >> number;
+          cout << number << endl;
+        } else if (!cin.fail()){
+          break;
+        }
+      }
+
       if (number < 0){
         cout << "The number of assignment you have entered is too small.\n";
       } else if (number > 10){
         cout << "The number of assignments you have entered is too large.\n";
-      } else if (!isnan(number)){
-        cout << "Please enter a number between 0 and 10\n";
-        number = readInt(prompt);
-      }
-        cout << !isnan(number) << true << endl;
-  } while (number < 0 || number > 10 && isnan(number) == true);
+      } 
+
+  } while (number < 0 || number > 10);
 
   
 
