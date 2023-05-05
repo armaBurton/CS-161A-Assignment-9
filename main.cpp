437841fd763467f3413468db70fa197bc076cd54
Arma Burton <arma@armaburton.com>
2023-05-04 17:30:50 -0700

    setup loop for individual assignments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include "welcomeText/welcomeText.cpp"
 #include "readInt/readInt.cpp"
 
@@ -8,11 +9,18 @@ using namespace std;
 
 int main () {
   int numberOfAssignments {};
+  double assignmentGrade {},
+         sumOfAssignmentGrade {};
+  ostringstream oss;
 
   welcomeText();
-    numberOfAssignments = readInt("Enter the number of assignments (0 to 10): ");
-
-
+  numberOfAssignments = readInt("Enter the number of assignments (0 to 10): ");
+  for(int i = 0; i < numberOfAssignments;i++){
+    oss << "Enter score " << i + 1 << ": ";
+    cin >> assignmentGrade;
+    sumOfAssignmentGrade += assignmentGrade;
+  }
+  cout << sumOfAssignmentGrade << endl;
 
   return 0;
 }
