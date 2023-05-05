58d729f410b70dc6efcd4a81a52ee4d6c64231e5
Arma Burton <arma@armaburton.com>
2023-05-05 14:21:16 -0700

    refactoring

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <sstream>
 #include "welcomeText/welcomeText.cpp"
 #include "readInt/readInt.cpp"
 
@@ -11,22 +10,25 @@ int main () {
   int numberOfAssignments {};
   double assignmentGrade {},
          sumOfAssignmentGrade {};
-  ostringstream oss;
   string templateLiteral {},
          queryConstructor {};
 
   welcomeText();
-  numberOfAssignments = readInt("Enter the number of assignments (0 to ", 10);
+  numberOfAssignments = readInt("Enter the number of assignments (0 to 10).");
   cout << endl;
-  for(int i = 0; i < numberOfAssignments;i++){
-    queryConstructor = "Enter score ";
-    queryConstructor += to_string(i + 1);
-    queryConstructor += " (0 to ";
-    assignmentGrade = readInt(queryConstructor, 4);
-    sumOfAssignmentGrade += assignmentGrade;
+  while(numberOfAssignments < 0 || numberOfAssignments > 10){
+    numberOfAssignments = readInt("Please enter a value between 0 - 10.");
   }
-  sumOfAssignmentGrade /= numberOfAssignments;
-  cout << sumOfAssignmentGrade << endl;
+
+  // for(int i = 0; i < numberOfAssignments;i++){
+  //   queryConstructor = "Enter score ";
+  //   queryConstructor += to_string(i + 1);
+  //   queryConstructor += " (0 to ";
+  //   assignmentGrade = readInt(queryConstructor, 4);
+  //   sumOfAssignmentGrade += assignmentGrade;
+  // }
+  // sumOfAssignmentGrade /= numberOfAssignments;
+  // cout << sumOfAssignmentGrade << endl;
 
   return 0;
 }
--- readInt/readInt.cpp
+++ readInt/readInt.cpp
@@ -5,33 +5,37 @@
 
 using namespace std;
 
-int readInt(string prompt, int max){
+int readInt(string prompt){
   
-  int number = -1;
-  bool checkNumber {};
-  string str = prompt;
-  str += to_string(max);
-  str += "): ";
-  do{
-      cout << str;
-      cin >> number;
-      while (1){
-        if (cin.fail()){
-          cin.clear();
-          cin.ignore(numeric_limits<streamsize>::max(),'\n');
-          cout << str;
-          cin >> number;
-        } else if (!cin.fail()){
-          break;
-        }
-      }
-      if (number < 0 || number > max){
-        cout << "You have entered an invalid value.";
-      }
-
-  } while (number < 0 || number > max);
-
-  
-
-  return number;
 }
+
+// int readInt(string prompt, int max){
+  
+//   int number = -1;
+//   bool checkNumber {};
+//   string str = prompt;
+//   str += to_string(max);
+//   str += "): ";
+//   do{
+//       cout << str;
+//       cin >> number;
+//       while (1){
+//         if (cin.fail()){
+//           cin.clear();
+//           cin.ignore(numeric_limits<streamsize>::max(),'\n');
+//           cout << str;
+//           cin >> number;
+//         } else if (!cin.fail()){
+//           break;
+//         }
+//       }
+//       if (number < 0 || number > max){
+//         cout << "You have entered an invalid value.";
+//       }
+
+//   } while (number < 0 || number > max);
+
+  
+
+//   return number;
+// }
