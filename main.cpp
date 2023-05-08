e9fdb536c70ec97e352e1c1baad09827161ea6fa
Arma Burton <arma@armaburton.com>
2023-05-08 14:15:03 -0700

    readInt has error checking

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ int main () {
   numberOfAssignments = readInt("Enter the number of assignments (0 to 10): ");
   cout << endl;
   while(numberOfAssignments < 0 || numberOfAssignments > 10){
+    cout << "User input is greater or lesser than allowed value.\n";    
     numberOfAssignments = readInt("Please enter a value between 0 - 10: ");
   }
 
--- readInt/readInt.cpp
+++ readInt/readInt.cpp
@@ -13,7 +13,7 @@ int readInt(string prompt){
     if (cin.fail()){
       cin.clear();
       cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      
+      cout << "Please enter a numerical value. ";
       cout << prompt;
       cin >> number;
     } else if (!cin.fail()){
@@ -23,34 +23,3 @@ int readInt(string prompt){
 
   return number;
 }
-
-// int readInt(string prompt, int max){
-  
-//   int number = -1;
-//   bool checkNumber {};
-//   string str = prompt;
-//   str += to_string(max);
-//   str += "): ";
-//   do{
-//       cout << str;
-//       cin >> number;
-//       while (1){
-//         if (cin.fail()){
-//           cin.clear();
-//           cin.ignore(numeric_limits<streamsize>::max(),'\n');
-//           cout << str;
-//           cin >> number;
-//         } else if (!cin.fail()){
-//           break;
-//         }
-//       }
-//       if (number < 0 || number > max){
-//         cout << "You have entered an invalid value.";
-//       }
-
-//   } while (number < 0 || number > max);
-
-  
-
-//   return number;
-// }
