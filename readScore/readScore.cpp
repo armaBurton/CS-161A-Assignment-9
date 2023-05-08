4b206e78e50df4dff3ce825fb6685cb2f5048daf
Arma Burton <arma@armaburton.com>
2023-05-08 16:41:10 -0700

    readScore logic needs work

--- readScore/readScore.cpp
+++ readScore/readScore.cpp
@@ -7,6 +7,17 @@ void readScore(string prompt, double &num){
 
   cout << prompt;
   cin >> scoreInput;
+  // while (1){
+  //   if (cin.fail()){
+  //     cin.clear();
+  //     cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  //     cout << "Please enter a numerical value. ";
+  //     cout << prompt;
+  //     cin >> scoreInput;
+  //   } else if (!cin.fail()){
+  //     break;
+  //   }
+  }
   while(scoreInput < 0 || scoreInput > 4){
     cout << "Score must be a value of 0 - 4.\n";
     cout << prompt;
