b261b6a77f97a9f70298d4b325e1e2ac1fecd645
Arma Burton <arma@armaburton.com>
2023-05-08 15:11:15 -0700

    updates, mostly comments

--- main.cpp
+++ main.cpp
@@ -14,27 +14,24 @@ int main () {
   string templateLiteral {},
          queryConstructor {};
 
+  //Welcome prompt
   welcomeText();
+
+  //query user for number of assignments that will be graded
   numberOfAssignments = readInt("Enter the number of assignments (0 to 10): ");
   cout << endl;
+  //if 10 > numberOfAssignments > 0, query again, repeat until correct value is entered.
   while(numberOfAssignments < 0 || numberOfAssignments > 10){
     cout << "User input is greater or lesser than allowed value.\n";    
     numberOfAssignments = readInt("Please enter a value between 0 - 10: ");
   }
 
+  //loop through numberOfAssignment for collection of assignment grades.
   for (int i = 0; i < numberOfAssignments; i++){
     queryConstructor = "Enter score ";
     queryConstructor += to_string(i + 1);
     queryConstructor += ": ";
     readScore(queryConstructor, sumOfAssignmentGrade);
-    cout << sumOfAssignmentGrade << endl;
-    //refactor to include readScore(string prompt, double &num) when reading scores inputted from user.
-    // assignmentGrade = readInt(queryConstructor);
-    // while (assignmentGrade < 0 || assignmentGrade > 4){
-    //   cout << "Score must should be a value 0 - 4, inclusive.\n";
-    //   assignmentGrade = readInt(queryConstructor);
-    // }
-    // sumOfAssignmentGrade += assignmentGrade;
   }
   
 
--- readScore/readScore.cpp
+++ readScore/readScore.cpp
@@ -9,6 +9,7 @@ void readScore(string prompt, double &num){
   cin >> scoreInput;
   while(scoreInput < 0 || scoreInput > 4){
     cout << "Score must be a value of 0 - 4.\n";
+    cout << prompt;
     cin >> scoreInput;
   }
   num += scoreInput;
