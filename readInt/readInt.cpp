efcb717c7c758020b02c02856e2e1f2b826a418f
Arma Burton <arma@armaburton.com>
2023-05-08 14:09:52 -0700

    error checking for non numerical values

--- readInt/readInt.cpp
+++ readInt/readInt.cpp
@@ -7,9 +7,50 @@ using namespace std;
 
 int readInt(string prompt){
   int number {};
-
   cout << prompt;
   cin >> number;
+  while (1){
+    if (cin.fail()){
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      
+      cout << prompt;
+      cin >> number;
+    } else if (!cin.fail()){
+      break;
+    }
+  }
 
   return number;
 }
+
+// int readInt(string prompt, int max){
+  
+//   int number = -1;
+//   bool checkNumber {};
+//   string str = prompt;
+//   str += to_string(max);
+//   str += "): ";
+//   do{
+//       cout << str;
+//       cin >> number;
+//       while (1){
+//         if (cin.fail()){
+//           cin.clear();
+//           cin.ignore(numeric_limits<streamsize>::max(),'\n');
+//           cout << str;
+//           cin >> number;
+//         } else if (!cin.fail()){
+//           break;
+//         }
+//       }
+//       if (number < 0 || number > max){
+//         cout << "You have entered an invalid value.";
+//       }
+
+//   } while (number < 0 || number > max);
+
+  
+
+//   return number;
+// }
